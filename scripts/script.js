146074a0f5425c1a86ce5d38666f93aedd8ceacc
unknown <cerwinroxas17@gmail.com>
2024-08-19 07:09:58 -0700

    fix responsiveness, add animation on load and resize some div

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 const contentHome = document.querySelector('#content-home');
 const aboutContainer = document.querySelector('#content-about');
 const contentTitle = document.querySelector('#content-title');
-const card2 = document.querySelector('.div2 .card');
 const contentContact = document.querySelector('#content-contact');
 const contentCertificates = document.querySelector('#content-certificates');
 const contentProjects = document.querySelector('#content-projects');
+const cardsContent = document.querySelectorAll('.card');
 
 const hideAllSections = () => {
     contentHome.style.display = 'none';
@@ -14,46 +14,59 @@ const hideAllSections = () => {
     contentProjects.style.display = 'none';
 };
 
-const resetCardStyles = () => {
-    card2.style.marginTop = '0rem';
-    card2.style.paddingBottom = '0rem';
+const applyAnimation = (element) => {
+    element.classList.add('animate');
+    element.addEventListener('animationend', () => {
+        element.classList.remove('animate');
+    }, { once: true });
 };
 
 const homePage = () => {
     hideAllSections();
-    resetCardStyles();
     contentHome.style.display = 'block';
     contentTitle.innerHTML = 'Welcome to CyyDev Portfolio!';
+
+    for (let i = 0; cardsContent.length; i++){
+        applyAnimation(cardsContent[i]);
+    }
 };
 
 const aboutPage = () => {
     hideAllSections();
-    resetCardStyles();
     aboutContainer.style.display = 'block';
-    card2.style.marginTop = '6rem';
     contentTitle.innerHTML = 'About Me';
+    
+    for (let i = 0; cardsContent.length; i++){
+        applyAnimation(cardsContent[i]);
+    }
 };
 
 const contactPage = () => {
     hideAllSections();
-    resetCardStyles();
     contentContact.style.display = 'block';
-    card2.style.marginTop = '4rem';
     contentTitle.innerHTML = 'Contact Me';
+   
+    for (let i = 0; cardsContent.length; i++){
+        applyAnimation(cardsContent[i]);
+    }
 };
 
 const certPage = () => {
     hideAllSections();
-    resetCardStyles();
     contentCertificates.style.display = 'block';
-    card2.style.paddingBottom = '2rem';
     contentTitle.innerHTML = 'Seminars & Training Certificates';
+   
+    for (let i = 0; cardsContent.length; i++){
+        applyAnimation(cardsContent[i]);
+    }
 };
 
 const projectPage = () => {
     hideAllSections();
-    resetCardStyles();
     contentProjects.style.display = 'block';
-    card2.style.paddingBottom = '2rem';
     contentTitle.innerHTML = 'Project Experience';
+   
+    for (let i = 0; cardsContent.length; i++){
+        applyAnimation(cardsContent[i]);
+    }
 };
